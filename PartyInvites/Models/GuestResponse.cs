91cfda6a71b531f820d679c7ccc35189268867aa
vnabet <vnabet@gmail.com>
2014-01-31 17:29:24 +0100

    first commit

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your Email adress")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your Phone number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
